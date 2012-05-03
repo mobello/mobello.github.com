186fe32277209cd9cf24fe604da05693c133c926
Lee Wang-soo <hivemind02@gmail.com>
2012-05-03 18:45:37 +0900

    update api

--- src/apps/bbc/main.js
+++
@@ -1,7 +0,0 @@
-$require('/list.js');
-
-$class('tau.bbc.Main').extend(tau.ui.SequenceNavigator).define({
-  init: function () {  
-    this.setRootController(new tau.bbc.ListController());
-  }
-});
