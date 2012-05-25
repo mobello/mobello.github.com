2b439549d75addbef51bbb002e74082d16942eea
Lee Wang-soo <hivemind02@gmail.com>
2012-05-25 13:33:27 +0900

    no comment

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function request_download() {
-	if ($('eula') != true) {
+	if ($('#eula').checked != true) {
 		alert('You have to agree to the EULA.')
 		return;
 	}
 	var url = "http://125.131.85.49/Mobello/web2/_res/_server/mobello_download.asp";
 
 	var data = {
-		_M_ID : $('email').text,
+		_M_ID : $('#email').value,
 		_M_DOMAIN : '',
 		_LANG : 'en' // ko, en
 	};
