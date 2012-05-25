205d2a76ea8174550f389610b8308f641a764442
Lee Wang-soo <hivemind02@gmail.com>
2012-05-25 13:25:43 +0900

    no comment

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
 function request_download() {
-	if($('eula') != true) {
+	if ($('eula') != true) {
 		alert('You have to agree to the EULA.')
 		return;
 	}
 	var url = "http://125.131.85.49/Mobello/web2/_res/_server/mobello_download.asp";
-		 
-		var data = {
-		  _M_ID: $('email').text,
-		  _M_DOMAIN: '',
-		  _LANG: 'en' // ko, en
-		};
-		 
+
+	var data = {
+		_M_ID : $('email').text,
+		_M_DOMAIN : '',
+		_LANG : 'en' // ko, en
+	};
+
 	$.ajax({
-		  type: 'GET'
-		  crossDomain: true,
-		  url: url,
-		  data: data,
-		  success:function(result) {
-				alert('success');
-			},
-			error:function(jqxhr, textStatus, errorThrown) {
-				alert('success');
-			},
-		}); 
+		type : 'GET',
+		crossDomain : true,
+		url : url,
+		data : data,
+		success : function(result) {
+			alert('success');
+		},
+		error : function(jqxhr, textStatus, errorThrown) {
+			alert('success');
+		},
+	});
 }
 
 function openBlog() {
-	if(getLanguage().indexOf('ko') !== -1) {
+	if (getLanguage().indexOf('ko') !== -1) {
 		window.open('http://mobello.tumblr.com/');
 	} else {
 		window.open('http://mobellojs.tumblr.com/');
 	}
 }
 function navigateToTutorial() {
-	if(getLanguage().indexOf('ko') !== -1) {
-		window.location.href="/tutorial1-ko/";
+	if (getLanguage().indexOf('ko') !== -1) {
+		window.location.href = "/tutorial1-ko/";
 	} else {
-		window.location.href="/tutorial1/";
+		window.location.href = "/tutorial1/";
 	}
 }
 function getLanguage() {
-	return  window.navigator.userLanguage || window.navigator.language;
+	return window.navigator.userLanguage || window.navigator.language;
 }
